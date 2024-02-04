4c3ee52cda709e5d1a1563855323ad590c7c175e
p1usminus <darealafrothunder@hotmail.com>
2024-02-04 01:47:55 +0000

    Remove last references to JS modules (*.jsm)

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,4 @@
-Components.utils.import("resource://gre/modules/AddonManager.jsm");
+const { CustomizableUI } = ChromeUtils.importESModule("resource:///modules/CustomizableUI.sys.mjs");
 
 var Globals = {};
 
@@ -78,7 +78,7 @@ async function startup(data, reason) {
   Services.scriptloader.loadSubScript("chrome://roomybookmarkstoolbar/content/prefs.js", temp, 'UTF-8');
   delete temp;
 
-  Components.utils.import("resource:///modules/CustomizableUI.jsm");
+  const { CustomizableUI } = ChromeUtils.importESModule("resource:///modules/CustomizableUI.sys.mjs");
 
   try {
     CustomizableUI.createWidget({
